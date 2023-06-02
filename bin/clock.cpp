bdc0ade4a4f7dc517dec5cedb3aa38dd8389ea8b
ap <ap@pop-os.localdomain>
2023-06-02 17:43:10 +0200

    Add cpp script for sneaky locking

---
+++ bin/clock.cpp
@@ -0,0 +1,121 @@
+#define _SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING
+#include <chrono>
+#include <cstdlib>
+#include <ctime>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <string>
+#include <thread>
+
+extern "C" {
+#include <X11/Xlib.h>
+#include <X11/Xresource.h>
+#include <X11/Xutil.h>
+#include <X11/extensions/Xrandr.h>
+}
+
+using namespace std;
+
+void takeScreenshot(const string &filename) {
+  Display *display = XOpenDisplay(NULL);
+  Window root = DefaultRootWindow(display);
+
+  XWindowAttributes attributes;
+  XGetWindowAttributes(display, root, &attributes);
+  int width = attributes.width;
+  int height = attributes.height;
+
+  XImage *image =
+      XGetImage(display, root, 0, 0, width, height, AllPlanes, ZPixmap);
+
+  cv::Mat screenshot(height, width, CV_8UC4, image->data);
+
+  cv::imwrite(filename, screenshot);
+
+  XDestroyImage(image);
+  XCloseDisplay(display);
+}
+
+void lockComputer(const string &bgName) {
+  string lockCommand = "i3lock -i " + bgName;
+  const char *command = lockCommand.c_str();
+  system(command);
+}
+
+void takeWebcamPicture(const string &directory) {
+  cv::VideoCapture camera(0);
+  if (!camera.isOpened()) {
+    cerr << "Failed to open the camera" << endl;
+    return;
+  }
+
+  cv::Mat frame;
+  camera >> frame;
+  camera.release();
+
+  string timestamp = to_string(time(nullptr));
+  string filename = directory + "/webcam_" + timestamp + ".jpg";
+  cv::imwrite(filename, frame);
+  cout << "Webcam picture saved: " << filename << endl;
+}
+
+bool monitorMouseMovement(const string &directory, int maxPictures) {
+  Display *display = XOpenDisplay(NULL);
+  Window root = DefaultRootWindow(display);
+
+  Window child;
+  int rootX, rootY, winX, winY;
+  unsigned int mask;
+
+  int pictureCounter = 0;
+  XQueryPointer(display, root, &root, &child, &rootX, &rootY, &winX, &winY,
+                &mask);
+  int lastX = rootX;
+  int lastY = rootY;
+  while (true) {
+    XQueryPointer(display, root, &root, &child, &rootX, &rootY, &winX, &winY,
+                  &mask);
+    if (rootX != lastX || rootY != lastY) {
+      takeWebcamPicture(directory);
+      pictureCounter++;
+      if (pictureCounter >= maxPictures) {
+        cout << "Maximum number of pictures reached. Stopping..." << endl;
+        XCloseDisplay(display);
+        return true;
+      }
+    }
+    lastX = rootX;
+    lastY = rootY;
+    this_thread::sleep_for(chrono::milliseconds(500));
+  }
+
+  XCloseDisplay(display);
+  return false;
+}
+
+int main() {
+  string directory = "/home/ap/Pictures"; // Change directory if needed
+
+  string backgroundPath = directory + "/background.png";
+  bool bgExists = std::filesystem::exists(backgroundPath);
+  if (!bgExists) {
+    takeScreenshot(backgroundPath);
+  }
+
+  lockComputer(backgroundPath);
+
+  int maxPictures = 5;
+  bool maxPicturesReached = monitorMouseMovement(directory, maxPictures);
+
+  if (maxPicturesReached) {
+    cout << "Exiting program after taking " << maxPictures << " pictures."
+         << endl;
+    return 0;
+  }
+
+  cin.get(); // Wait for any key press to terminate the script
+
+  return 0;
+}
--- etc/zsh/.zshenv
+++ etc/zsh/.zshenv
@@ -5,3 +5,4 @@ export ETC_DIR=$LOCAL_DIR/etc
 export PATH=$PATH:$WORK_DIR/.local/bin
 export ZDOTDIR=$WORK_DIR/.local/etc/zsh #needs to contain everything regarding zsh
 
+. "$HOME/.cargo/env"
